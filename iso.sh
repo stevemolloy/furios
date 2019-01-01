c4b0fe2c793165f0ed1b18891211ae4872b893fd
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2019-01-01 23:04:56 +0100

    Rename to furios

--- iso.sh
+++ iso.sh
@@ -6,10 +6,10 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cp sysroot/boot/furios.kernel isodir/boot/furios.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "myos" {
-	multiboot /boot/myos.kernel
+menuentry "furios" {
+	multiboot /boot/furios.kernel
 }
 EOF
-grub-mkrescue -o myos.iso isodir
+grub-mkrescue -o furios.iso isodir
